001f8ae8cb74e8e6adeec58a274621af1fd9c939
LUDO TAKEBE <33525582+LUDO-TAKEBE@users.noreply.github.com>
2020-09-28 01:30:57 +0900

    Add files via upload

---
+++ ld/build/ld.build/Debug/ALL_BUILD.build/Script-10F4BB2E5781405CBB09CB91.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/LUDO/Documents/Max\ 8/Packages/ld/build -f /Users/LUDO/Documents/Max\ 8/Packages/ld/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
